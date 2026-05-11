699a422edece2d1f889de4f4a89072ab117d8a6a
ma7moudmo7med <ma7moudmo7med1@gmail.com>
2026-05-11 14:54:13 +0300

    feat(backend): initialize backend service and docker orchestration

--- smart-camera-service/obj/Debug/net11.0/SmartCameraService.AssemblyInfo.cs
+++ smart-camera-service/obj/Debug/net11.0/SmartCameraService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCameraService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f60cd8ead995d809ada6f87d3241326c59f2ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCameraService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCameraService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
